3e6f9089a07af949a87e47a89bde1b6ebdc96227
Bulat Safiullin (Akvelon INC) <v-bsafiullin@microsoft.com>
2021-11-24 20:57:36 +0300

    feat import drinkItemsData from redux to CardsList

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 
 import Grid from "@mui/material/Grid";
+import { useSelector, useDispatch } from "react-redux";
 
 import { cardsData } from "../../data/cardsData";
+import { RootState } from "../../redux/store";
 import { Card } from "../Card";
 
 import { StyledGridItem } from "./styles";
 
 export const CardsList = () => {
+  const drinksDataR = useSelector(
+    (state: RootState) => state.drinkItems.drinkItems
+  );
   const [drinksData, setDrinksData] = useState(cardsData);
 
   const deleteDrinkItem = (id: string) => {
@@ -21,7 +26,7 @@ export const CardsList = () => {
 
   return (
     <Grid container spacing={2}>
-      {drinksData.map((drinksItem) => (
+      {drinksDataR.map((drinksItem) => (
         <StyledGridItem item xs={12} key={drinksItem.id}>
           <Card
             drinksItem={drinksItem}
